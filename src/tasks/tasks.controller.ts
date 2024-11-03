987e2298bf6be174b1eded1c0f10c500fe7769ce
Khalid Imran <ekhalid090@gmail.com>
2024-11-03 10:07:48 +0600

    added create task functionality

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from '../insterfaces/tasks/task.interface';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+  @Get()
+  getTasks(): Task[] {
+    return this.taskService.getTasks();
+  }
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.taskService.createTask(createTaskDto);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from '../insterfaces/tasks/task.interface';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { v4 as uuid } from 'uuid';
+import { Status } from '../enums/tasks/status.enum';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: Status.PENDING,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+  getTasks() {
+    return this.tasks;
+  }
+}
